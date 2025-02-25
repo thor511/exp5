da99cbeeb1cd0cf43789608d4a5cf45d0f1e25c3
aids <example@gmail.com>
2025-02-25 10:02:10 +0530

    changes in model

--- model.py
+++ model.py
@@ -13,21 +13,21 @@ lr = LinearRegression()
 ####
 # Load and split data
 for _ in range(10):
-rng = np.random.RandomState(_)
-x = 10 * rng.rand(1000).reshape(-1,1)
-y = 2 * x - 5 + rng.randn(1000).reshape(-1,1)
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=50)
-########################################################################
-######
-# Fitting the model
-lr.fit(X_train, y_train)
-y_preds = lr.predict(X_test)
-test_mse = mean_squared_error(y_test, y_preds )
-average_mse = np.mean(test_mse)
-print(f'MSE Result: { test_mse}')
+	rng = np.random.RandomState(_)
+	x = 10 * rng.rand(1000).reshape(-1,1)
+	y = 2 * x - 5 + rng.randn(1000).reshape(-1,1)
+	X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=50)
+	########################################################################
+	######
+	# Fitting the model
+	lr.fit(X_train, y_train)
+	y_preds = lr.predict(X_test)
+	test_mse = mean_squared_error(y_test, y_preds )
+	average_mse = np.mean(test_mse)
+	print(f'MSE Result: { test_mse}')
 print("Average Mean Squared Error:", average_mse)
 with open('metrics.txt', 'w') as outfile:
-outfile.write(f'Mean Squared Error = {average_mse}.')
+	outfile.write(f'Mean Squared Error = {average_mse}.')
 # Plotting the training data
 plt.scatter(X_train, y_train, color='blue', label='Training data')
 # Plotting the testing data
